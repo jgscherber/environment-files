c2d9a752f3111a856045ad7ac779651fd7fea5f0
Jacob Scherber <jgscherber@gmail.com>
2022-12-21 12:42:42 -0500

    fix bad quotes and OneNote UI

--- PowerShell/Scripts/Toggle-Zenbook-Touchscreen/Toggle-Zenbook-Touchscreen.ps1
+++ PowerShell/Scripts/Toggle-Zenbook-Touchscreen/Toggle-Zenbook-Touchscreen.ps1
@@ -1,6 +1,6 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] “Administrator”))
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
-    Write-Warning “You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!”
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
     Break
 }
 
