ef3733fdf37a188f84b3950ae36f928f4f732e98
Jacob Scherber <jgscherber@gmail.com>
2023-02-12 16:18:12 -0500

    file moved

--- StartupTask/Startup_launch.ps1
+++ StartupTask/Startup_launch.ps1
@@ -1,7 +1,7 @@
 # Always run as admin
 
 $StartUpCommands = @(
-    "& 'C:\Program Files\AutoHotkey\AutoHotkey.exe' C:\Develop\environment-files\keymove.ahk",
+    "& 'C:\Program Files\AutoHotkey\AutoHotkey.exe' C:\Develop\environment-files\AutoHotKey\keymove.ahk",
     "& 'C:\Program Files\Ditto\Ditto.exe'"
 )
 
