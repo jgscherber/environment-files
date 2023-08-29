c5aac118005406d3bd24fdecdf7751a343334aec
Jacob Scherber <jgscherber@gmail.com>
2023-08-29 08:30:31 -0400

    fix startup scripts

--- StartupTask/RegisterStartupTask.ps1
+++ StartupTask/RegisterStartupTask.ps1
@@ -1,18 +1,18 @@
-param([string] environmentFilesPath)
+param([string] $environmentFilesPath)
 
 $startupFolder = "Startup"
 $startupFile = "Startup.ps1"
 
-$starupFolderPath = "$($environmentFilesPath)\$($startupFolder);
-$startupFilePath = "$($starupFolderPath)\$($startupFile)";
+$starupFolderPath = $environmentFilesPath + "\" + $startupFolder;
+$startupFilePath = $starupFolderPath + "\" + $startupFile;
 
 # Create Task
 $taskName = "Startup utilities"
-$action = New-ScheduledTaskAction "powershell.exe $startupPath" -WorkingDirectory $startupFolderPath
+$action = New-ScheduledTaskAction "powershell.exe $startupFilePath" -WorkingDirectory $environmentFilesPath
 $trigger = New-ScheduledTaskTrigger -AtLogon
 $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel Highest
 
 # TODO task properties
 $taskSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 00:00:00
-Register-ScheduledTask $taskName -Description "Run Geneva Monitoring Agent on system startup" -Action $action -Trigger $trigger -Principal $principal -Settings $taskSettings
+Register-ScheduledTask $taskName -Description "Run startup script on system startup" -Action $action -Trigger $trigger -Principal $principal -Settings $taskSettings
 Start-ScheduledTask -TaskName $taskName
--- StartupTask/Startup_launch.ps1
+++ StartupTask/Startup_launch.ps1
@@ -1,7 +1,7 @@
 # Always run as admin
 
 $StartUpCommands = @(
-    "Set-Location ..\AutoHotKey; & 'C:\Program Files\AutoHotkey\AutoHotkey.exe' .\keymove.ahk",
+    "& 'C:\Program Files\AutoHotkey\AutoHotkey.exe' .\AutoHotKey\keymove.ahk",
     "& 'C:\Program Files\Ditto\Ditto.exe'"
 )
 
