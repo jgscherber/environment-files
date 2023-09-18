32a7bfdf4a7136a1446105e44185d6be8b6372d2
Jacob Scherber <jgscherber@gmail.com>
2023-09-18 10:28:16 -0400

    line sorting and code extensions

--- Chocolately/New-Environment.ps1
+++ Chocolately/New-Environment.ps1
@@ -11,12 +11,14 @@ if(-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Admi
     throw "Must be run as Admin";
 }
 
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-# iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+function Test-CommandExists([string] $commandName)
+{
+  $command = Get-Command $commandName -ErrorAction SilentlyContinue;
+  return $null -ne $command;
+}
 
-$command = Get-Command choco -ErrorAction SilentlyContinue
-if ($null -eq $command)
+
+if (-not (Test-CommandExists "choco"))
 {
   Write-Host "choco command not found, installing Chocolately";
   winget install chocolatey.chocolatey;
@@ -27,43 +29,76 @@ if ($null -eq $command)
 
 #### Chocolately ####
 Write-Host "Installing application";
-choco install notion -y;
+choco install 7zip.install -y;
 choco install anki -y;
+choco install autohotkey -y;
+choco install chocolateygui -y;
+choco install ditto -y;
+choco install everything --params "/folder-context-menu /run-on-system-startup /client-service" -y;
+choco install Firefox --params "/NoDesktopShortcut /RemoveDistributionDir" -y;
+choco install git.install -y;
+choco install github-desktop -y;
+choco install klogg -y;
+choco install notion -y;
+choco install obsidian -y;
 choco install paint.net -y;
+choco install passwordsafe -y;
+choco install powershell-core -y;
+choco install python3 -y;
+choco install remote-desktop-client -y;
 choco install spotify -y;
 choco install vscode -y;
-choco install remote-desktop-client -y;
-choco install klogg -y;
-choco install autohotkey -y;
-choco install windirstat -y;
-choco install passwordsafe -y;
-choco install x-mouse-button-control -y;
-choco install ditto -y;
-choco install 7zip.install -y;
-choco install python3 -y;
-choco install git.install -y;
 choco install win-vind -y;
-choco install github-desktop -y;
-choco install Firefox --params "/NoDesktopShortcut /RemoveDistributionDir" -y;
-choco install everything --params "/folder-context-menu /run-on-system-startup /client-service" -y;
-choco install obsidian -y;
-choco install chocolateygui -y;
+choco install windirstat -y;
+choco install x-mouse-button-control -y;
 
 if ($environment -eq "Home")
 {
   choco install adobereader -y;
-  choco install vlc -y;
   choco install visualstudio2022community -y;
+  choco install vlc -y;
 }
 
 if ($environment -eq "Work")
 {
-  choco install visualstudio2022enterprise -y;
   choco install sql-server-management-studio -y;
+  choco install visualstudio2022enterprise -y;
 }
 
-
-# PowerShell
-choco install powershell-core -y;
+######### Dependent Installs ############
+$Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 
+# VS Code
+if (Test-CommandExists "code")
+{
+  code --install-extension dvirtz.parquet-viewer
+  code --install-extension GitHub.copilot
+  code --install-extension GitHub.copilot-chat
+  code --install-extension hediet.vscode-drawio
+  code --install-extension hnw.vscode-auto-open-markdown-preview
+  code --install-extension janisdd.vscode-edit-csv
+  code --install-extension johnpapa.vscode-peacock
+  code --install-extension khaeransori.json2csv
+  code --install-extension L13RARY.l13-diff
+  code --install-extension mark-wiemer.vscode-autohotkey-plus-plus
+  code --install-extension ms-dotnettools.csharp
+  code --install-extension ms-dotnettools.vscode-dotnet-runtime
+  code --install-extension ms-mssql.data-workspace-vscode
+  code --install-extension ms-mssql.mssql
+  code --install-extension ms-mssql.sql-bindings-vscode
+  code --install-extension ms-mssql.sql-database-projects-vscode
+  code --install-extension ms-python.python
+  code --install-extension ms-python.vscode-pylance
+  code --install-extension ms-toolsai.jupyter
+  code --install-extension ms-toolsai.jupyter-keymap
+  code --install-extension ms-toolsai.jupyter-renderers
+  code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+  code --install-extension ms-toolsai.vscode-jupyter-slideshow
+  code --install-extension ms-vscode.powershell
+  code --install-extension msazurermtools.azurerm-vscode-tools
+  code --install-extension redhat.vscode-xml
+  code --install-extension rosshamish.kuskus-kusto-language-server
+  code --install-extension rosshamish.kuskus-kusto-syntax-highlighting
+  code --install-extension yzhang.markdown-all-in-one
+}
