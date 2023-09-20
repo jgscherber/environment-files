23e83d6f2de2997905375ea31fe2f97cba9426dc
Jacob Scherber <jgscherber@gmail.com>
2023-09-20 13:04:41 -0400

    work kb install

--- Chocolately/New-Environment.ps1
+++ Chocolately/New-Environment.ps1
@@ -65,6 +65,7 @@ if ($environment -eq "Work")
   choco install sql-server-management-studio -y;
   choco install visualstudio2022enterprise -y;
   choco install dotnetcore-2.1-sdk -y;
+  Get-WUInstall -KBArticleID KB5027702 -AcceptAll -IgnoreReboot;
 }
 
 ######### Dependent Installs ############
