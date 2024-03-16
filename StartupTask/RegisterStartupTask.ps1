1f0844d40e8ec445f6c86305e46c680e876400bf
Jacob Scherber <jgscherber@gmail.com>
2024-03-16 14:27:11 -0400

    move to powershell

--- StartupTask/RegisterStartupTask.ps1
+++ StartupTask/RegisterStartupTask.ps1
@@ -4,19 +4,19 @@ if (-not $environmentFilesPath)
     throw "Missing environmentFilesPath";
 }
 
+$taskName = "Startup utilities"
 $startupFolder = "StartupTask"
 $startupFile = "Startup_launch.ps1"
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 
 $starupFolderPath = $environmentFilesPath + "\" + $startupFolder;
 $startupFilePath = $starupFolderPath + "\" + $startupFile;
 
 # Create Task
-$taskName = "Startup utilities"
 $action = New-ScheduledTaskAction "powershell.exe $startupFilePath" -WorkingDirectory $environmentFilesPath
-$trigger = New-ScheduledTaskTrigger -AtLogon
-$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel Highest
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-WindowStyle Hidden -File '$startupFilePath' -WorkingDirectory '$startupFolder'"
+$trigger = New-ScheduledTaskTrigger -AtLogOn -User "$currentUser"
 
-# TODO task properties
-$taskSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 00:00:00
-Register-ScheduledTask $taskName -Description "Run startup script on system startup" -Action $action -Trigger $trigger -Principal $principal -Settings $taskSettings
-Start-ScheduledTask -TaskName $taskName
+$taskSettings = = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew -DisallowStartIfOnBatteries $false -StopIfGoingOnBatteries $true -AllowHardTerminate $true -StartWhenAvailable $false -RunOnlyIfNetworkAvailable $false -AllowStartOnDemand $true -Enabled $true -Hidden $false -RunOnlyIfIdle $false -DisallowStartOnRemoteAppSession $false -UseUnifiedSchedulingEngine $true -WakeToRun $true -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -Priority 7
+
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $taskSettings -Description 'Run startup script on system startup' -User "$currentUser" -RunLevel Highest
