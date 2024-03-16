f6d727b4c8e5e488063e31787dc45b719677f110
Jacob Scherber <jgscherber@gmail.com>
2024-03-16 15:52:12 -0400

    update schedule task

--- StartupTask/RegisterStartupTask.ps1
+++ StartupTask/RegisterStartupTask.ps1
@@ -9,14 +9,13 @@ $startupFolder = "StartupTask"
 $startupFile = "Startup_launch.ps1"
 $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 
-$starupFolderPath = $environmentFilesPath + "\" + $startupFolder;
-$startupFilePath = $starupFolderPath + "\" + $startupFile;
+$startupFolderPath = $environmentFilesPath + "\" + $startupFolder;
+$startupFilePath = $startupFolderPath + "\" + $startupFile;
 
 # Create Task
-$action = New-ScheduledTaskAction "powershell.exe $startupFilePath" -WorkingDirectory $environmentFilesPath
-$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-WindowStyle Hidden -File '$startupFilePath' -WorkingDirectory '$startupFolder'"
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-WindowStyle Hidden -File `"$startupFilePath`"" -WorkingDirectory $startupFolderPath
 $trigger = New-ScheduledTaskTrigger -AtLogOn -User "$currentUser"
 
-$taskSettings = = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew -DisallowStartIfOnBatteries $false -StopIfGoingOnBatteries $true -AllowHardTerminate $true -StartWhenAvailable $false -RunOnlyIfNetworkAvailable $false -AllowStartOnDemand $true -Enabled $true -Hidden $false -RunOnlyIfIdle $false -DisallowStartOnRemoteAppSession $false -UseUnifiedSchedulingEngine $true -WakeToRun $true -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -Priority 7
+$taskSettings = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -WakeToRun -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -Priority 7
 
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $taskSettings -Description 'Run startup script on system startup' -User "$currentUser" -RunLevel Highest
