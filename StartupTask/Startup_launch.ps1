d27e98440a8ced2f9e0e266df2c1364beb8955c4
Jacob Scherber <jgscherber@gmail.com>
2024-03-16 14:21:11 -0400

    working startup script

--- StartupTask/Startup_launch.ps1
+++ StartupTask/Startup_launch.ps1
@@ -1,10 +1,27 @@
 # Always run as admin
 
 $StartUpCommands = @(
-    "& 'C:\Program Files\AutoHotkey\AutoHotkey.exe' .\AutoHotKey\keymove.ahk",
+    "& 'C:\Program Files\AutoHotkey\AutoHotkey.exe' /restart '..\AutoHotKey\keymove.ahk'",
     "& 'C:\Program Files\Ditto\Ditto.exe'"
 )
 
-$StartUpCommands | ForEach-Object { Invoke-Expression $_ -ErrorAction SilentlyContinue};
+# Create a new event source if it doesn't exist
+$source = "StatupLaunch"
+if (![System.Diagnostics.EventLog]::SourceExists($source)) {
+    [System.Diagnostics.EventLog]::CreateEventSource($source, "Application")
+}
 
+function Write-Event($entryType, $message)
+{
+    Write-EventLog -LogName Application -Source $source -EntryType $entryType -EventId 1 -Message $message
+}
 
+$StartUpCommands | ForEach-Object {
+    try {
+        Write-Event "Info" "Starting $_"
+        Start-Process -NoNewWindow -FilePath PowerShell -WorkingDirectory $PSScriptRoot -ArgumentList "$_"
+    } catch {
+        # Write the error to the event log
+        Write-Event "Error" $_.Exception.Message
+    }
+}
